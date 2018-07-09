60dc60d6e5ec3c005f41ca8feef9c50c88caef93
Brogan Miner <BroganMiner22@gmail.com>
2018-07-09 15:48:57 -0700

    Update deprecated ip location finder

--- php/getUserLocation.php
+++ php/getUserLocation.php
@@ -1,6 +1,6 @@
 <?php
   // Collect some user data (approximate ip, location, etc)
-  $location = file_get_contents("http://freegeoip.net/json/".$_SERVER['REMOTE_ADDR']);
+  $location = file_get_contents("http://api.ipstack.com/".$_SERVER['REMOTE_ADDR']."?access_key=YOUR_ACCESS_KEY");
   // Print info as part of the HTTP reply.
   print $location;
  ?>
