3a0852723af472ed2a98d55d559ff708b2bd4c5b
jackrwoods <jackrwoods@gmail.com>
2018-08-22 08:00:14 -0700

    Fixed getUserLocation.php but need to re-write server routes for compatibility with our mySQL server.

--- php/getUserLocation.php
+++ php/getUserLocation.php
@@ -1,6 +1,6 @@
 <?php
   // Collect some user data (approximate ip, location, etc)
-  $location = file_get_contents("http://api.ipstack.com/".$_SERVER['REMOTE_ADDR']."?access_key=YOUR_ACCESS_KEY");
+  $location = file_get_contents("http://api.ipstack.com/".$_SERVER['REMOTE_ADDR']."?access_key=b9c68aebc8a30c958ed8e258a7a9a22e");
   // Print info as part of the HTTP reply.
   print $location;
  ?>
