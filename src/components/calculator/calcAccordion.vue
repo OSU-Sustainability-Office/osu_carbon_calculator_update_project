cf3252a798f12dfab75a7a32b7e6ac2abd67ad19
Jack Woods <jackrwoods@gmail.com>
2018-11-26 09:11:48 -0800

    Transferring work from laptop to work pc.

--- src/components/calculator/calcAccordion.vue
+++ src/components/calculator/calcAccordion.vue
@@ -1,14 +1,35 @@
 <template>
+<el-col :span="24" v-loading="loading">
 
-  <el-col :span="24">
-  </el-col>
+  <el-collapse v-model="catAccordion" accordion>
 
+    <el-collapse-item v-for="category in categories" :key="category.categoryID" :title="category.title" :name="category.categoryID">
+      <list questionData="category.questions[0]" />
+
+    </el-collapse-item>
+
+  </el-collapse>
+
+</el-col>
 </template>
 
 <script>
+import list from '@/components/calculator/questions/list'
 
 export default {
-  name: 'calcAccordion'
+  name: 'calcAccordion',
+  data () {
+    return {
+      catAccordion: '0'
+    }
+  },
+  computed: {
+    categories () { return this.$store.getters['calculator/categories'] },
+    loading () { return (this.$store.getters['calculator/categories'] > 0) }
+  },
+  components: {
+    list
+  }
 }
 </script>
 
---
+++ src/components/calculator/questions/list.vue
@@ -0,0 +1,30 @@
+<template>
+
+  <el-form ref="form" :model="form" label-width="120px" v-display="questionData.visible">
+    {{questionData.text}} <br />
+    blah
+    <el-select placeholder="Select One">
+      <el-option
+        v-for="(choice, index) in questionData.input.values"
+        :key="index"
+        :label="choice.val"
+        :value="choice.val"
+        :coef="choice.coef">
+      </el-option>
+    </el-select>
+
+  </el-form>
+
+</template>
+
+<script>
+
+export default {
+  name: 'list',
+  props: ['questionData']
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
