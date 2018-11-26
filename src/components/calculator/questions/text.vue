d61877e77403e7bed80ec8c59af3fbabcc402711
jackrwoods <jackrwoods@gmail.com>
2018-11-26 10:15:03 -0800

    Text question vue component done.

---
+++ src/components/calculator/questions/text.vue
@@ -0,0 +1,22 @@
+<template>
+
+  <div class="question">
+
+    {{questionData.text}} <br />
+
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'text',
+  props: {
+    'questionData': Object
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
