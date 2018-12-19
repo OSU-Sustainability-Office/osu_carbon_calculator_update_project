984a701ac3441eaf6d2c5e87e6eb1bc5592a7537
Jack Woods <jackrwoods@gmail.com>
2018-12-19 12:32:01 -0800

    Update navbar styling.

--- src/components/navBar.vue
+++ src/components/navBar.vue
@@ -1,6 +1,17 @@
+<!--
+@Author: Jack Woods
+@Date:   2018-11-27T13:45:59-08:00
+@Filename: navBar.vue
+@Last modified by:   Jack Woods
+@Last modified time: 2018-12-19T12:13:04-08:00
+@Copyright: 2018 Oregon State University
+-->
+
 <template>
 <el-menu :default-active="activeIndex" mode="horizontal" @select="handleSelect">
-  <el-menu-item index="1"><svgLogo class="logo"/>
+  <el-menu-item index="0"><svgLogo class="logo"/>
+  </el-menu-item>
+  <el-menu-item index="1">Home
   </el-menu-item>
   <el-menu-item v-if="loggedOut" index="2">Login</el-menu-item>
   <el-submenu v-else index="3">
@@ -23,7 +34,9 @@ export default {
       activeIndex: '1',
       administrator: this.$store.getters['user/administrator'],
       loginLink: 'https://api.sustainability.oregonstate.edu/auth/login?returnURI=' + window.location,
-      logoutLink: 'https://api.sustainability.oregonstate.edu/auth/logoutRedirect'
+      logoutLink: 'https://api.sustainability.oregonstate.edu/auth/logoutRedirect',
+      officeHomepageLink: 'http://sustainability.oregonstate.edu/',
+      dashboardLink: 'https://dashboard.sustainability.oregonstate.edu/'
     }
   },
   computed: {
@@ -34,8 +47,17 @@ export default {
   methods: {
     handleSelect (key, keyPath) {
       switch (key) {
+        case '0':
+          window.location = this.officeHomepageLink
+          break
+        case '1':
+          this.router.go('#')
+          break
         case '2':
           window.location = this.loginLink
+          break
+        case '3-2':
+          window.location = this.dashboardLink
           break
         case '3-4':
           window.location = this.logoutLink
