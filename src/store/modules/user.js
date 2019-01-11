1fd58e508d0d8f916319aa488a55f5c9e67323c3
Jack Woods <jackrwoods@gmail.com>
2019-01-10 16:00:33 -0800

    Remove unnecessary console.logh

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -3,7 +3,7 @@
  * @Date:   2018-11-27T13:45:59-08:00
  * @Filename: user.js
  * @Last modified by:   Jack Woods
- * @Last modified time: 2019-01-08T12:14:02-08:00
+ * @Last modified time: 2019-01-10T16:00:11-08:00
  * @Copyright: 2018 Oregon State University
  */
 
@@ -54,7 +54,6 @@ export default {
     // Removes one historical data entry
     removeHistData (state, index) {
       state.data.splice(index, 1)
-      console.log(index)
     }
   },
   actions: {
