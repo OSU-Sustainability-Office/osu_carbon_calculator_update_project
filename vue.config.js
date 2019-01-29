947b87eb24369d34571bf45bd03720c108317795
Jack Woods <jackrwoods@gmail.com>
2019-01-29 13:39:14 -0800

    Trying to fix the build process to enable compatibility with legacy browsers... and edge...

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,43 @@
+/**
+ * @Author: Jack Woods
+ * @Date:   2018-11-27T13:45:59-08:00
+ * @Email:  jackrwoods@gmail.com
+ * @Filename: vue.config.js
+ * @Last modified by:   Jack Woods
+ * @Last modified time: 2019-01-29T13:32:02-08:00
+ * @Copyright: 2019 Oregon State University
+ */
+
+const babelEnvDeps = require('webpack-babel-env-deps')
+
 // vue.config.js
 module.exports = {
-  configureWebpack: {
-    plugins: [
-    ]
-  },
   chainWebpack: (config) => {
+    // Transpile dependencies
+    // config.module
+    //   .rule('transpileDependencies')
+    //   .test(/\.js$/)
+    //   .exclude(babelEnvDeps.exclude())
+    //   .use('babel-loader')
+    //   .loader('babel-loader')
+    //   .end()
+    config.merge({
+      module: {
+        rules: [
+          {
+            test: /\.js$/,
+            exclude: [
+              babelEnvDeps.exclude() // returns /node_modules(?!(/|\\)(detect-indent|request|...)(/|\\))/
+            ],
+            use: {
+              loader: 'babel-loader'
+            }
+          }
+        ]
+      }
+    })
+
+    // Load and render SVG images
     const svgRule = config.module.rule('svg')
     svgRule.uses.clear()
     svgRule
