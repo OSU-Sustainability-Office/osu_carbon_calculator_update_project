cdd2a92e1dc5e371f5a6932d7490d0b933703eea
Jack Woods <jackrwoods@gmail.com>
2019-10-08 13:35:36 -0700

    Drop support for IE 11.

--- public/legacy/js/tabling.js
+++
@@ -1,9 +0,0 @@
-/* Open when someone clicks on the span element */
-function openOverlay(input) {
-    document.getElementById(input).style.height = "100%";
-}
-
-/* Close when someone clicks on the "x" symbol inside the overlay */
-function closeOverlay(input) {
-    document.getElementById(input).style.height = "0%";
-}
